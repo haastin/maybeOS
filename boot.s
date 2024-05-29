c38e626b379e80d46a98ca822f2321d890c37975
Austin Harris <austinharris729@gmail.com>
2024-05-29 06:30:06 -0700

    added stack support for kernel code

--- boot.s
+++ boot.s
@@ -12,6 +12,26 @@
 .long FLAGS
 .long CHECKSUM
 
+#allocating stack in bss saves space in this file's representation in the fs
+.section .bss 
+.align 16 #System V ABI expects the stack to be 16 byte aligned
+stack_bottom: #this will be at a lower address since it is higher in the file, which is why it's the bottom
+.skip 16384
+stack top:
+
+.intel_syntax noprefix
+
+.section .text
+.global _start #chatGPT says convention is to use .global first
+.type _start, @function
+_start:
+
+    mov esp, stack_top
+
+/*at this point we have a code and data segment passed from GRUB, but no GDT*/
+
+basic_gdt_start:
+
 
 
 
